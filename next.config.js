c6e8143a332cb3f3dc42f8556c8539d6187771cc
eveisking <kingeve0202@gmail.com>
2023-03-20 23:48:39 +0100

    finsihed the header section of the app

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['rb.gy', 'upload.wikimedia.org', 'clipartmax.com'],
+  },
 }
 
 module.exports = nextConfig
---
+++ src/components/Banner.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <section className='w-full h-[100vh] bg-[#ffffff] flex'>
+        <h1>Hello Apple</h1>
+    </section>
+  )
+}
+
+export default Banner
