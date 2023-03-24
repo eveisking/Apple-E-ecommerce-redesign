0f4f691e65764786f989c3818b5a60efe9d0fbd6
eveisking <kingeve0202@gmail.com>
2023-03-24 04:25:45 +0100

    fectched products from sanity backend

---
+++ utils/fetchProducts.ts
@@ -0,0 +1,11 @@
+export const fecthProducts = async () => {
+    const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/getProducts`
+        );
+
+        const data = await res.json();
+        const products: Product[] = data.products;
+
+        return products;
+        
+};
