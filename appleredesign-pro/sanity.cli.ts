3096b764c4f8bb122aa55fc9d499d9fe6cbee1ef
eveisking <kingeve0202@gmail.com>
2023-04-06 10:55:53 +0100

    sanity configs and schemas

---
+++ appleredesign-pro/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'rpe2tl8v',
+    dataset: 'production'
+  }
+})
---
+++ appleredesign-pro/schemas/index.ts
@@ -0,0 +1,16 @@
+
+
+// Document types
+
+import page from './documents/page'
+
+import categories from './documents/categories'
+import productVariant from './documents/productVariant'
+import productDoc from './documents/productDoc'
+
+const documents = [productDoc, page, categories]
+
+
+
+
+export const schemaTypes = [...documents]
