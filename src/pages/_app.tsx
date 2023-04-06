1102003647af9513ac5160e3e8499da8b1d575de
eveisking <kingeve0202@gmail.com>
2023-04-06 02:39:01 +0100

    react-redux provide to hook the app with store

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { store } from '@/redux/store'
+import { Provider } from 'react-redux';
+import toast, { Toaster } from 'react-hot-toast';
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Provider store={store}>
+          <Toaster />
+         <Component {...pageProps} />
+         </Provider>
 }
