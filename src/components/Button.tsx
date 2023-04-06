67891d237f3795e358f476925b06526e3b1c2aba
eveisking <kingeve0202@gmail.com>
2023-04-06 02:25:23 +0100

    added reusable button component with props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -18,7 +18,7 @@ const Button = ({title, onClick, width, loading, padding, noIcon}: props) => {
     onClick={onClick}
     
     >
-        <span className='absolute bottom-0 right-0 mb-8 mr-5 h-20 w-8 transition-x-1
+        <span className='absolute bottom-0 right-0 mb-4 mr-2 h-20 w-8 transition-x-1
         rotate-45 transform bg-white opacity-10 transition-all duration-300
         ease-out group-hover:translate-x-0 '></span>
         <span className='absolute top-0 left-0 mt-8 ml-5 h-8 w-20 transition-x-1
