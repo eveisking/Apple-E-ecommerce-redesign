f05445179606a205912abf87fcad93dd12613ec4
eveisking <kingeve0202@gmail.com>
2023-04-06 02:36:10 +0100

    react redux hook read data from store and dispatch

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,14 +1,22 @@
 import { ShoppingCartIcon } from '@heroicons/react/outline';
 import Image from 'next/image';
 import React from 'react'
+import { useDispatch } from 'react-redux';
 import { urlFor } from '../../sanity';
+import { addToCart } from '@/redux/cartSlice';
+import toast, { Toaster } from 'react-hot-toast';
 
 interface Props {
     product: Product;
 }
 function Products({product}: Props) {
+const dispatch = useDispatch();
+  const addItemsToCart = () => {
+    dispatch(addToCart(product));
 
-  const addItemsToBasket = () => {
+    toast.success(`${product.title} has been added to the cart`, {
+      position: "bottom-center"
+    });
     
   }
   return (
@@ -17,9 +25,8 @@ function Products({product}: Props) {
     bg-[#35383C] p-4 md:h-[500px] md:w-[390px] sm:h-[400px] sm:w-[230px] md:p-10 mb-4 
     shadow-lg sm:gap-'>
       <div className='relative h-64 w-full md:h-72'>
-        <Image src={urlFor(product.image[0]).url()} fill 
-               alt={product.title}
-               className="object-contain"/>
+        <Image src={urlFor(product.image[0]).url()} fill alt={product.title}
+               className='object-contain' />
       </div>
       <div className='flex flex-1 items-center justify-between space-x-3'>
       <div className='text-white space-y-3 text-xl md:text-2xl sm:text-sm'>
@@ -27,7 +34,7 @@ function Products({product}: Props) {
         <p>Price: ${product.price}</p>
         </div>
         <div className='cartButton'
-           onClick={addItemsToBasket}>
+           onClick={addItemsToCart}>
           <ShoppingCartIcon className='h-8 w-8 text-white'></ShoppingCartIcon>
         </div>
       </div>
